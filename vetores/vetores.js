9a9fb242f42966baf865662d5f0c4c9236bab90a
Marcos Santos <marcos.ts201128@gmail.com>
2024-08-24 18:58:56 -0300

    adicionado cores aos vetores infectados

--- vetores/vetores.js
+++ vetores/vetores.js
@@ -1,7 +1,9 @@
 
-function Vector( posicoes ) {
-	this.array = []
-	this.somatorio = 0
+class Vector {
+	constructor(posicoes) {
+		this.array = []
+		this.somatorio = 0
+	}
 }
 
 // Função que gera um número aleatório
@@ -33,44 +35,3 @@ function gerarVetores(qtd_vetores, qtd_posicoes){
 	
 	return vetores
 }
-
-
-
-
-function validarVetor( vetor, limite ){
-	let contador = 0
-
-	for ( const value in vetor ) {
-		console.log( contador )
-	}
-
-	return true
-}
-
-/* ------------------------------------------------------ */
-// Exemplo 1: Gerar um vetor com 30 posições
-/* ------------------------------------------------------ */
-//let vetor = gerarVetor(30)
-//console.log("Exibindo o único vetor criado: ")
-//console.log( vetor )
-
-
-/* ------------------------------------------------------ */
-// Exemplo 2: Gerar três vetores e cada um tem 25 posições
-/* ------------------------------------------------------ */
-//let vetores = gerarVetores(3, 25)
-// Aqui irá exibir todos os vetores
-//console.log("Exibindo os três vetores criados: ")
-//console.log( vetores )
-
-// Aqui irá exibir apenas o segundo vetor.
-//console.log("Exibindo apenas o segundo dos três vetores criados: ")
-//console.log( vetores[1] )
-
-// Aqui irá exibir apenas o quarto valor do segundo vetor.
-//console.log("Exibindo apenas o quarto valor do segundo vetor: ")
-//console.log( vetores[1][3] )
-
-/* ------------------------------------------------------ */
-// Exemplo 3: Calculo dos vetores
-/* ------------------------------------------------------ */
